6673a2f26eadacd7d39c29d60a7a61c6dfa6bf52
Lucas Pomar <lucaspomar@outlook.com>
2025-08-01 14:52:47 -0300

    Generate new secret key and add to env

--- backend/todoApp/todoApp/settings.py
+++ backend/todoApp/todoApp/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z8amx*8ga^-hdd(ois_c(28oj*mw3_h3!*gr+0m=tm%=+0u_j_'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
