3315b85a6f8ff0c6de1d9df53b14a4cc0c94fdd4
Lucas Pomar <lucaspomar@outlook.com>
2025-08-01 17:22:47 -0300

    Create Todo model

--- backend/todoApp/api/models.py
+++ backend/todoApp/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    due_date = models.DateField(blank=False)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
