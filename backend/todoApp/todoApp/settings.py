2e25fa4fe4474050947bafaba0af5dcf8931acb0
Lucas Pomar <lucaspomar@outlook.com>
2025-08-01 21:35:59 -0300

    Add simple JWT for authentication

--- backend/todoApp/todoApp/settings.py
+++ backend/todoApp/todoApp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'drf_spectacular',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,9 @@ ROOT_URLCONF = 'todoApp.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 SPECTACULAR_SETTINGS = {
--- backend/todoApp/todoApp/urls.py
+++ backend/todoApp/todoApp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include('api.urls')),
 ]
