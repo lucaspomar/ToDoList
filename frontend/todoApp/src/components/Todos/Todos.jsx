73db4d8f66222b360712bb132697ed824733de21
Lucas Pomar <lucaspomar@outlook.com>
2025-08-02 19:45:43 -0300

    Todos header

--- frontend/todoApp/src/components/Todos/Todos.jsx
+++ frontend/todoApp/src/components/Todos/Todos.jsx
@@ -1,9 +1,33 @@
 import './Todos.css'
+import user_icon from '../../assets/person.png'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Todos = () => {
+
+    const navigate = useNavigate();
+
+    const token = sessionStorage.getItem('token');
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/');
+        }
+    })
+
   return (
-    <div> 
-        Todos
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>Lista de Tarefas</div>
+            <div className='underline'></div>
+            <div className='inputs'>
+                <div className='input'>
+                    <img src={user_icon} alt='' />
+                    <input type='text' placeholder='Busca' />
+                </div>
+            </div>
+            <div className='underline'></div>
+        </div> 
     </div>
   )
 }
